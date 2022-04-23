df78e322c1f2b3230945442f516051fc9c46af25
Kishan Kandu <iamkishan067@gmail.com>
2022-04-23 22:46:07 +0530

    base_url_changed
    Changed base Url

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,4 +1,4 @@
-let baseUrl = "http://3.111.116.146:8080";
+// let baseUrl = "http://3.111.116.146:8080";
 
-// let baseUrl = "http://localhost:8080"
+let baseUrl = "http://localhost:8080";
 export default baseUrl;
